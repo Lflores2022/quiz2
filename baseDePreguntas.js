761bd0e7d6339d064e6425101bdcd9c84909ab6e
Lflores2022 <launchx03687@innovaccion.mx>
2022-06-25 08:12:59 -0500

    subiendo quiz dos

---
+++ baseDePreguntas.js
@@ -0,0 +1,147 @@
+baseDePreguntas = [
+   /* {
+      pregunta: `¿Quién pintó la Mona Lisa?`,
+      imagen: `https://i.ibb.co/0MvMH3V/mona-lisa-louvre-134fb54a-550x821.jpg`,
+      respuesta: `Leonardo da Vinci`,
+      distractores: [`Miguel Ángel`, `Dali`, `Picasso`],
+      ayuda: `También pintó la última cena`,
+      ayudaImg: `https://i.ibb.co/wgZZvKc/obras-de-arte-leonardo-da-vinci-google-498711013-153997319-1024x576.jpg`,
+    },
+    {
+      pregunta: `¿Cuánto es 2+2?`,
+      respuesta: `4`,
+      distractores: [`3`, `2`, `1`],
+      ayuda: `es lo mismo que 2*2`,
+      ayudaImg: `https://i.ibb.co/k1WgGW2/descarga.jpg`,
+    },*/
+    {
+        pregunta: 'Es el lobulo que se encarga del razonamiento, movimiento, emociones y lenguaje',
+        distractores: [
+            'temporal',
+            'frontal',
+            'occipital',
+            'paretial'
+        ],
+        respuesta: 'frontal'
+    },
+    {
+        pregunta: 'Es el lobulo que se encarga del tacto, gusto, olfato, presion y temperatura',
+        distractores: [
+            'temporal',
+            'frontal',
+            'occipital',
+            'paretial'
+        ],
+        respuesta: 'paretial'
+    },
+    {
+        pregunta: 'Es el lobulo que se encarga de la vision',
+        distractores: [
+            'temporal',
+            'frontal',
+            'occipital',
+            'paretial'
+        ],
+        respuesta: 'occipital'
+    },
+    {
+        pregunta: 'Es el obulo encargado de la audicion, memoria y aprendizaje',
+        distractores: [
+            'temporal',
+            'frontal',
+            'occipital',
+            'paretial'
+        ],
+        respuesta: 'temporal'
+    },
+    {
+        pregunta: 'este neurotransmisor genera, te mantiene alerta y te brinda motivacion',
+        distractores: [
+            'endorfina',
+            'dopamina',
+            'glutamato',
+            'adrenalina'
+        ],
+        respuesta: 'adrenalina'
+    },
+    {
+        pregunta: 'este neurotransmisor te da inhibición del dolor y te da placer',
+        distractores: [
+            'endorfina',
+            'dopamina',
+            'glutamato',
+            'adrenalina'
+        ],
+        respuesta: 'endorfina'
+    },
+    {
+        pregunta: 'este neurotransmisor se encarga del estado de animo, deseo sexual y te matiene alerta',
+        distractores: [
+            'endorfina',
+            'noradrenalina',
+            'oxitocina',
+            'adrenalina'
+        ],
+        respuesta: 'noradrenalina'
+    },
+    {
+        pregunta: 'este neurotransmisor te brinda memoria, aprendizaje y estimulación muscular',
+        distractores: [
+            'adrenalina',
+            'oxitocina',
+            'glutamato',
+            'acelticolina'
+        ],
+        respuesta: 'acelticolina'
+    },
+    {
+        pregunta: 'este neurotransmisor equilibra el control de emociones',
+        distractores: [
+            'endorfina',
+            'dopamina',
+            'cocaina',
+            'adrenalina'
+        ],
+        respuesta: 'dopamina'
+    },
+    {
+        pregunta: 'este neurotransmisor emite memoria de aprendizaje y razonamiento',
+        distractores: [
+            'serotonina',
+            'gaba',
+            'glutamato',
+            'adrenalina'
+        ],
+        respuesta: 'glutamato'
+    },
+    {
+        pregunta: 'este neurotransmisor crea una balance emocional, regula el apetito, deseo sexual, temperatura y sueño',
+        distractores: [
+            'endorfina',
+            'noradrenalina',
+            'serotonina',
+            'adrenalina'
+        ],
+        respuesta: 'serotonina'
+    },
+    {
+        pregunta: 'este neurotransmisor crea concentracion, sueño y ansiedad',
+        distractores: [
+            'endorfina',
+            'gaba',
+            'oxitocina',
+            'glutamato'
+        ],
+        respuesta: 'gaba'
+    },
+    {
+        pregunta: 'este neurotransmisor se relaciona con vínculos afectivos, intimidad, confianza y fidelidad',
+        distractores: [
+            'endorfina',
+            'gaba',
+            'oxitocina',
+            'glutamato'
+        ],
+        respuesta: 'oxitocina'
+    },
+  ];
